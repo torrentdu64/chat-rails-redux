2c7d09ebccdb2b9b057e6d4974554a0aad8b5558
torrent benoit <torrentdu64@gmail.com>
2018-08-14 13:42:48 +1200

    add registrations controller for devise controllers Channels

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::MessagesController < ApplicationController
+
+  before_action :set_channel
+
   def index
+    messages = @channel.messages.order('content ASC')
+    render json: messages
   end
 
   def create
   end
+
+  private
+
+  def set_channel
+    @channel = Channel.find_by(name: params[:channel_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # devise_for :users
+ devise_for :users
 
-  namespace :api, defaults: { format: :json } do
+namespace :api, defaults: { format: :json } do
   namespace :v1 do
     resources :channels, only: [] do
       resources :messages, only: [ :index, :create ]
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   end
 end
 
-resources :channels, only: [ :show ]
-root to: 'channels#show'
+ resources :channels, only: [ :show ]
+ root to: 'channels#show'
 end
